9262878015c4f24366a6722b53408d2f00dc5777
Степан <79278993666@ya.ru>
2023-04-09 21:44:46 +0400

    Init

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,26 +14,23 @@
         let url = form.action;
         const formData = new FormData(form);  
 
-        if( (formData.get("username")).length > 0 ){ // проверка на спам
-          return false;    
+        console.log(formData.get("required"));
+
+        if( (formData.get("required")).length > 0 ){ // проверка на спам
+          return;    
         }
         
         const params = Array.from(formData);  
         let query = new URLSearchParams(params);
         const queryString = query.toString();
 
-        if( !validationForm(formData) ){
-          return;
-        }
-
-        form.checkValidity();
-
         let req = new XMLHttpRequest();
         req.open("GET", url + '?' + queryString, false);
         req.send(null);
         console.log(req);
         if(req.status == 200){
-          form.classList.add('was-validated');
+          // form.classList.add('was-validated');
+          form.reset();
         }   
         
       }, false)
